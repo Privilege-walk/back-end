760cb81cce3369d9050b900fc3b9c5a715d11549
Suraj S Jain <surajsjain@hotmail.com>
2022-03-11 12:34:15 -0600

    Renamed main API

--- user_mgmt/urls.py
+++ user_mgmt/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from user_mgmt.controllers.apis import SayHello
+from user_mgmt.controllers.main_page_hello_API import SayHello
 
 urlpatterns = [
     path('', SayHello.as_view(), name='hello'),
