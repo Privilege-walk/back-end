30ba1f88e0f4f3451721828dce5cc8fdb2ee2c12
Suraj S Jain <surajsjain@hotmail.com>
2022-03-11 14:00:50 -0600

    Login API

--- user_mgmt/urls.py
+++ user_mgmt/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .controllers.main_page_hello_API import SayHello
-from .controllers.auth_apis import SignUp
+from .controllers.auth_apis import *
 
 urlpatterns = [
     path('', SayHello.as_view(), name='hello'),
     path('auth/signup/', SignUp.as_view(), name='signup'),
+    path('auth/login/', Login.as_view(), name='login'),
 ]
