ee8ac375146d246d7fa143675f5010c708dfae66
Suraj S Jain <surajsjain@hotmail.com>
2022-03-11 14:00:40 -0600

    Unauth access

--- user_mgmt/controllers/auth_apis.py
+++ user_mgmt/controllers/auth_apis.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
 
+from django.contrib import auth
+from rest_framework.authtoken.models import Token
+
+from rest_framework.permissions import AllowAny
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class SignUp(APIView):
+
+    permission_classes = [AllowAny]
+
     def post(self, request):
         in_data = request.data
 
@@ -43,3 +51,46 @@ class SignUp(APIView):
                 "created": "success"
             }
         )
+
+class Login(APIView):
+
+    permission_classes = [AllowAny]
+
+    def get(self, request, format=None):
+        return Response(
+            {
+                "status": False
+            }
+        )
+
+    def post(self, request, format=None):
+        data = request.data
+
+        username = data['username']
+        password = data['password']
+
+        try:
+            user = User.objects.get(username=username)
+            if user.check_password(password):
+                auth.login(request, user)
+                token, _ = Token.objects.get_or_create(user=user)
+                return Response(
+                    {
+                        "status": True,
+                        "token": token.key
+                    }
+                )
+
+            else:
+                return Response(
+                    {
+                        "status": False
+                    }
+                )
+
+        except:
+            return Response(
+                {
+                    "status": False
+                }
+            )
