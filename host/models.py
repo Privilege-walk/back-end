6798a5db7d3b11c7872400b16d0335bddff59268
Suraj S Jain <surajsjain@hotmail.com>
2022-03-21 18:20:06 -0500

    Host side models

--- host/models.py
+++ host/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+
 # Create your models here.
+class Event(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    status = models.CharField(max_length=50)  # Choices: Created, Running, Ended
+
+
+class Question(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+
+
+class AnswerChoice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    value = models.IntegerField()
