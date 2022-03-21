d2067f044aaa7cc7159b4efd8bffe3ede2973e45
Suraj S Jain <surajsjain@hotmail.com>
2022-03-21 18:46:16 -0500

    Response model

--- walk/models.py
+++ walk/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from user_mgmt.models import AnonymousParticipant
+from host.models import AnswerChoice
+
+
+class Response(models.Model):
+    participant = models.ForeignKey(AnonymousParticipant, on_delete=models.CASCADE)
+    answer = models.ForeignKey(AnswerChoice, on_delete=models.CASCADE)
