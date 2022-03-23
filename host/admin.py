be240c5989d8d3008a35f56c16abdd7dcd9d9192
Suraj S Jain <surajsjain@hotmail.com>
2022-03-23 16:17:55 -0500

    Enabled admin editing

--- host/admin.py
+++ host/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Event, Question, AnswerChoice
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(Question)
+admin.site.register(AnswerChoice)
