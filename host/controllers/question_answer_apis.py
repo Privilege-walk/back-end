017c6a3c3f917479242e6e3608f2cf48869804be
Suraj S Jain <surajsjain@hotmail.com>
2022-03-24 19:27:47 -0500

    Event details with questions and choices API

--- host/controllers/question_answer_apis.py
+++ host/controllers/question_answer_apis.py
@@ -4,14 +4,13 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from host.models import Question, AnswerChoice, Event
+from host.serializers import hostless, parentless_questions_and_answers
 
 
 class AddQuestion(APIView):
-
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-
         in_data = request.data
 
         question = Question.objects.create(
@@ -32,3 +31,52 @@ class AddQuestion(APIView):
                 "id": question.id,
             }
         )
+
+
+class EventQuestionAnswers(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        print(request.query_params)
+        event_id = request.query_params.get('event_id', None)
+
+        if event_id is None:
+            return Response(
+                {
+                    "message": "Please enter an event ID"
+                },
+                status=500
+            )
+
+        try:
+            event_in_focus = Event.objects.get(id=event_id)
+
+        except:
+            return Response(
+                {
+                    "message": "The event ID that you've entered is not in the records"
+                },
+                status=400
+            )
+
+        # Initializing the output dictionary with the event details
+        event_details = hostless.HostlessEventSerializer(event_in_focus).data
+
+        # Iterating over the questions in the event
+        event_questions = Question.objects.filter(event=event_in_focus)
+        questions_list = []
+
+        for question in event_questions:
+            question_data = {
+                "id": question.id,
+                "description": question.description
+            }
+
+            choices = parentless_questions_and_answers.ParentlessAnswerSerializer(AnswerChoice.objects.filter(question=question).order_by('-value'), many=True).data
+            question_data["choices"] = choices
+
+            questions_list.append(question_data)
+
+        event_details["questions"] = questions_list
+
+        return Response(event_details)
--- host/urls.py
+++ host/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from .controllers.event_apis import AllEvents, CreateEvent
-from .controllers.question_answer_apis import AddQuestion
+from .controllers.question_answer_apis import AddQuestion, EventQuestionAnswers
 
 urlpatterns = [
     path('events/all/', AllEvents.as_view(), name="getAllEvents"),
     path('events/create/', CreateEvent.as_view(), name="createNewEvent"),
     path('qa/create/', AddQuestion.as_view(), name="addQuestion"),
+    path('qa/eventwise_qas/', EventQuestionAnswers.as_view(), name="fullEventDetails"),
 ]
