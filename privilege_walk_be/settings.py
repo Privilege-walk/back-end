1acc777975426e60c0b7d2c5430703e4a804bc56
Suraj S Jain <surajsjain@hotmail.com>
2022-04-03 14:57:19 -0500

    AWS Database

--- privilege_walk_be/settings.py
+++ privilege_walk_be/settings.py
@@ -107,6 +107,18 @@ if run_type == "heroku":
         }
     }
 
+elif run_type == "aws":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv("DB_NAME"),
+            'USER': os.getenv("DB_USERNAME"),
+            'PASSWORD': os.getenv("DB_PASSWORD"),
+            'HOST': os.getenv("DB_HOST"),
+            'PORT': os.getenv("DB_PORT"),
+        }
+    }
+
 elif run_type == "docker_compose":
     DATABASES = {
         'default': {
