8cb6766d1eb73fe43ff1e066e046eb327e501d9e
Suraj S Jain <surajsjain@hotmail.com>
2022-04-10 15:51:05 -0500

    Question move tests

--- walk/tests/test_ws_consumer.py
+++ walk/tests/test_ws_consumer.py
@@ -3,29 +3,47 @@ import json
 from django.test import TestCase
 from channels.testing import WebsocketCommunicator
 
-from privilege_walk_be.asgi import application\
-
+from privilege_walk_be.asgi import application
 
 class QAConsumerTest(TestCase):
     def setUp(self) -> None:
         pass
 
-    async def test_connectivity(self):
+    async def test_channel(self):
 
-        self.communicator = WebsocketCommunicator(application, "/ws/walk/qa_control/12/")
-        connected, subprotocol = await self.communicator.connect()
+        ### Testing Connectivity
+
+        communicator = WebsocketCommunicator(application, "/ws/walk/qa_control/12/")
+        connected, subprotocol = await communicator.connect()
         self.assertEqual(connected, True)
 
         # Testing the initial active user count change
         expected = {
-                'meant_for': 'all',
-                'type': 'active_user_count',
-                'data': {
-                    'n_active_users': 1,
-                }
+            'meant_for': 'all',
+            'type': 'active_user_count',
+            'data': {
+                'n_active_users': 1,
             }
+        }
 
-        raw_message = await self.communicator.receive_from()
+        raw_message = await communicator.receive_from()
         message = json.loads(raw_message)
 
+        self.assertEqual(message, expected)
+
+
+        ### Testing the question move orders
+
+        # Host side
+        await communicator.send_to(json.dumps({
+            "type": "question_move"
+        }))
+
+        # Participant side
+        expected = {
+            "meant_for": "participants",
+            "type": "question_move",
+        }
+        raw_message = await communicator.receive_from()
+        message = json.loads(raw_message)
         self.assertEqual(message, expected)
