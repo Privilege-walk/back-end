abd987d5cb109dd5753cd2c2ead00a320c63e417
Suraj S Jain <surajsjain@hotmail.com>
2022-04-10 16:54:20 -0500

    Test case for answer choice sending

--- walk/tests/test_ws_consumer.py
+++ walk/tests/test_ws_consumer.py
@@ -1,13 +1,47 @@
 import json
 
-from django.test import TestCase
+from django.test import TransactionTestCase
 from channels.testing import WebsocketCommunicator
 
 from privilege_walk_be.asgi import application
 
-class QAConsumerTest(TestCase):
+from user_mgmt.models import AnonymousParticipant
+from host.models import Event, Question, AnswerChoice
+from django.contrib.auth.models import User
+
+class QAConsumerTest(TransactionTestCase):
     def setUp(self) -> None:
-        pass
+        host_user = User.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='howdy1234'
+        )
+
+        eve = Event.objects.create(
+            host=host_user,
+            name='Birthday',
+            status='Created',
+        )
+
+        ques = Question.objects.create(
+            event=eve,
+            description='Do you like cake?'
+        )
+
+        ans = AnswerChoice.objects.create(
+            question=ques,
+            description='yes',
+            value=1
+        )
+
+        self.answer_id = ans.id
+
+        anno_part = AnonymousParticipant.objects.create(
+            event=eve
+        )
+
+        anno_part.save()
+        self.participant_code = anno_part.unique_code
 
     async def test_channel(self):
 
@@ -47,3 +81,32 @@ class QAConsumerTest(TestCase):
         raw_message = await communicator.receive_from()
         message = json.loads(raw_message)
         self.assertEqual(message, expected)
+
+
+        ### Testing the answer choice messages
+
+        # Participant Side
+        await communicator.send_to(
+            json.dumps({
+                "type": "answer_choice",
+                "data": {
+                    "participant_code": self.participant_code,
+                    "answer_choice_id": self.answer_id
+                }
+            })
+        )
+
+        # Everyone gets a responded count
+        expected = {
+            "meant_for": "all",
+            "type": "answer_count",
+            "data": {
+                "n_users_answered": 1
+            }
+        }
+
+        raw_message = await communicator.receive_from()
+        message = json.loads(raw_message)
+        self.assertEqual(message, expected)
+
+        await communicator.disconnect()
