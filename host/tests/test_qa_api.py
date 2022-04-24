9e9e38d5ab2789c06e8b74288fff5fb818663361
Suraj S Jain <surajsjain@hotmail.com>
2022-04-24 12:34:32 -0500

    Removed auth tests

--- host/tests/test_qa_api.py
+++ host/tests/test_qa_api.py
@@ -116,13 +116,13 @@ class ViewQuestionsTestCase(TestCase):
         resp = self.client.post('/host/qa/create/', content_type='application/json', data=user_data, **{'HTTP_AUTHORIZATION':'Token '+ self.user_auth_token.key})
 
     def test_successful_get_questions(self):
-        resp = self.client.get('/host/qa/eventwise_qas/?event_id='+str(self.eventId), **{'HTTP_AUTHORIZATION':'Token '+ self.user_auth_token.key})
+        resp = self.client.get('/host/qa/eventwise_qas/?event_id='+str(self.eventId)) #, **{'HTTP_AUTHORIZATION':'Token '+ self.user_auth_token.key})
         response = resp.json()
         assert response["id"] == self.eventId
         assert response["questions"][0]["description"] == "The question's title goes here"
 
     def test_failure_get_questions_no_eventid(self):
-        resp = self.client.get('/host/qa/eventwise_qas/', **{'HTTP_AUTHORIZATION':'Token '+ self.user_auth_token.key})
+        resp = self.client.get('/host/qa/eventwise_qas/') #, **{'HTTP_AUTHORIZATION':'Token '+ self.user_auth_token.key})
         
         expected_out = {
             "message": "Please enter an event ID"
@@ -131,20 +131,10 @@ class ViewQuestionsTestCase(TestCase):
         self.assertEqual(resp.data, expected_out)
 
     def test_failure_get_questions_wrong_eventid(self):
-        resp = self.client.get('/host/qa/eventwise_qas/?event_id='+str('12345'), **{'HTTP_AUTHORIZATION':'Token '+ self.user_auth_token.key})
+        resp = self.client.get('/host/qa/eventwise_qas/?event_id='+str('12345'))#, **{'HTTP_AUTHORIZATION':'Token '+ self.user_auth_token.key})
         
         expected_out = {
             "message": "The event ID that you've entered is not in the records"
         }
-
-        self.assertEqual(resp.data, expected_out)
-
-    def test_failure_get_questions(self):
-
-        expected_out = {
-            "detail": "Authentication credentials were not provided."
-        }
-
-        resp = self.client.post('/host/qa/eventwise_qas/')
 
         self.assertEqual(resp.data, expected_out)
